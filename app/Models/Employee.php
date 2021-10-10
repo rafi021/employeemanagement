767a0f53b2c425e6cc033798d010c3b4c9bdfbdc
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2021-10-11 00:17:19 +0600

    Model fillables

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'last_name',
+        'first_name',
+        'middle_name',
+        'address',
+        'department_id',
+        'country_id',
+        'city_id',
+        'state_id',
+        'zip_code',
+        'birthdate',
+        'date_hired',
+    ];
 }
